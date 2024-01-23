872022c63f1526ef74dd4b19519c60de1a9eda70
YENRU <yenyen120886@gmail.com>
2024-01-23 16:47:23 +0800

    作業1更新

--- Controllers/ApiController.cs
+++ Controllers/ApiController.cs
@@ -12,13 +12,54 @@ namespace Fuen31Site.Controllers
         }
         public IActionResult Index()
         {
-            return View();
+            System.Threading.Thread.Sleep(5000);
+            return Content("<h2>Content,你好</h2>", "text/plain", System.Text.Encoding.UTF8);
         }
+
 
         public IActionResult Cities()
         {
-           var cities = _dbContext.Addresses.Select(a=>a.City).Distinct();
-           return Json(cities);
+            var cities = _dbContext.Addresses.Select(a => a.City).Distinct();
+            return Json(cities);
         }
+        public IActionResult Districts(string city)
+        {
+            var districts = _dbContext.Addresses.Where(a => a.City == city).Select(a => a.SiteId).Distinct();
+            return Json(districts);
+        }
+
+        public IActionResult Avatar(int id = 1)
+        {
+            Member? member = _dbContext.Members.Find(id);
+            if (member != null)
+            {
+                byte[] img = member.FileData;
+                return File(img, "image/jpeg");
+            }
+
+            return NotFound();
+        }
+
+        public IActionResult Register(string name, int age = 26)
+        {
+            if (string.IsNullOrEmpty(name))
+            {
+                name = "Guest";
+            }
+            return Content($"Hello {name}, You are {age} years old.");
+        }
+        public IActionResult First()
+        {
+            
+            return View();
+        }
+
+        public IActionResult Address()
+        {
+
+            return View();
+        }
+
+        
     }
 }
