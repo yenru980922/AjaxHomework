c0cd5d6abce802f6f2b650955f443faeb495408a
YENRU <yenyen120886@gmail.com>
2024-01-23 09:17:12 +0800

    加入專案檔案。

---
+++ Controllers/ApiController.cs
@@ -0,0 +1,24 @@
+﻿using Fuen31Site.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Fuen31Site.Controllers
+{
+    public class ApiController : Controller
+    {
+        private readonly MyDBContext _dbContext;
+        public ApiController(MyDBContext dbContext)
+        {
+            _dbContext = dbContext;
+        }
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult Cities()
+        {
+           var cities = _dbContext.Addresses.Select(a=>a.City).Distinct();
+           return Json(cities);
+        }
+    }
+}
